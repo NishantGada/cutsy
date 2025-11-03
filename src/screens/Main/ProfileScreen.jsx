9dc51d2e22551a9a7526951f373c277aea1b0cb9
NishantGada <nishant.gada160@gmail.com>
2025-11-03 17:32:52 -0500

    fix (profile): corrects safearea padding

--- src/screens/Main/ProfileScreen.jsx
+++ src/screens/Main/ProfileScreen.jsx
@@ -35,7 +35,7 @@ export default function ProfileScreen() {
   ];
 
   return (
-    <SafeAreaView style={[styles.container, { backgroundColor: theme.colors.background }]}>
+    <SafeAreaView style={[styles.container, { backgroundColor: theme.colors.background }]} edges={['top']}>
       <ScrollView showsVerticalScrollIndicator={false}>
         {/* Profile Header */}
         <LinearGradient
